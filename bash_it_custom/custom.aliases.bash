aa0d2063c3bc2946ef318d27b7a3982e64acef5d
Dom Hutton <dom.hutton@gmail.com>
2018-11-19 09:48:26 +1000

    ADD: git log alias

--- bash_it_custom/custom.aliases.bash
+++ bash_it_custom/custom.aliases.bash
@@ -9,15 +9,29 @@ alias ga="git add --all"
 alias gc="git commit -m"
 alias gp="git push"
 alias gb="git checkout -b"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # Code
-alias code="code --disable-gpu --verbose "
+alias code="code --disable-gpu --new-window "
 
 # GitHub
 # pr bug,enhancement,cost-saving,ready-for-review
 pr() {
     hub pull-request -a dombo -l $1 | xargs firefox --new-tab
 }
+
+# tmux
+# tmuxinator_start tanda-core <- for default session
+# tmuxinator_start tanda-core migration-library <- for named session
+tmuxinator_start() {
+    if [ -z "$2" ]; then SESSION_NAME=""; else SESSION_NAME="-n $2"; fi 
+    tmuxinator start $1 $SESSION_NAME
+}
+alias tanda-core="tmuxinator_start tanda-core "
+alias txl='tmux ls'
+alias txa='tmux a -t'
+alias txk='tmux kill-session -t'
+alias txka='tmux kill-server'
 
 # Clipboard
 alias pbcopy="xclip -selection clipboard"
